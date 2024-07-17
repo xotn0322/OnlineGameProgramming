876f9f3d094d38f96e3cb307622c66015202ece0
Hoguma <xotn0322@gmail.com>
2024-07-18 03:39:04 +0900

    cctv회전 복구

---
+++ Assets/Script/Enemy/New_CCTV_Control.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_CCTV_Control : MonoBehaviour
+{
+    public float rotationSpeed = 30f;
+    public float maxAngle = 45f;
+    public GameObject rader;
+
+    private float currentRotation = 0f;
+    private Vector3 initRotaion;
+    private bool rotatingClockwise = true;
+
+
+    private void Start()
+    {
+        initRotaion = transform.localRotation.eulerAngles;
+    }
+    void Update()
+    {
+        if (!rader.GetComponent<CCTV_Collider>().IsDetected())
+        {
+            RotateCCTV();
+        }
+    }
+
+    void RotateCCTV()
+    {
+        float rotationStep = rotationSpeed * Time.deltaTime;
+
+        if (rotatingClockwise)
+        {
+            currentRotation += rotationStep;
+            if (currentRotation > maxAngle)
+            {
+                currentRotation = maxAngle;
+                rotatingClockwise = false;
+            }
+        }
+        else
+        {
+            currentRotation -= rotationStep;
+            if (currentRotation < -maxAngle)
+            {
+                currentRotation = -maxAngle;
+                rotatingClockwise = true;
+            }
+        }
+
+        transform.localRotation = Quaternion.Euler(initRotaion.x, initRotaion.y + currentRotation, initRotaion.z);
+    }
+}
